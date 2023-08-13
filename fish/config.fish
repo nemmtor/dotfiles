264c4825bcd7ea15d262dbdad19d9062239d1a0e
nemmtor <kacper0witas@gmail.com>
2023-08-13 14:07:44 +0200

    experimenting with path

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,15 @@
 if status is-interactive
-    # PATH
-    export NVM_DIR="$HOME/.nvm"
-    set PATH $HOME/.meteor $PATH
-    set -gx GPG_TTY (tty)
+    # PATHS
+    export NVM_PATH="$HOME/.nvm"
+    export METEOR_PATH="$HOME/.meteor"
     export CARGO_PATH="$HOME/.cargo/bin"
     export GO_PATH="$HOME/go/bin"
     export WEZTERM_PATH="/Applications/WezTerm.app/Contents/MacOS"
     export VSCODE_PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-    export PATH="$CARGO_PATH:$PNPM_HOME:$NVM_DIR:$GO_PATH:$WEZTERM_PATH:$VSCODE_PATH:$PATH"
+    set -x PATH $NVM_PATH:$METEOR_PATH:$CARGO_PATH:$GO_PATH:$WEZTERM_PATH:$VSCODE_PATH:$PATH
+
+    # GPG TTY
+    set -gx GPG_TTY (tty)
 
     # git commands aliases
     alias gaa="git add -A"
@@ -34,16 +36,15 @@ if status is-interactive
     alias home="cd ~"
     alias de="cd ~/Desktop"
     alias pro="cd ~/Projects"
+    alias configs="cd ~/.config"
+    alias mybrain="cd ~/my-brain"
 
     # config files
     alias nvimrc="vim ~/.config/nvim/init.lua"
     alias fishrc="vim ~/.config/fish/config.fish"
     alias weztermrc="vim ~/.config/wezterm/wezterm.lua"
 
-    # some utils
-    alias noswap="rm ~/.local/state/nvim/swap/*"
-
-    # some crazy utils
+    # utils
     alias work="timer 25m && terminal-notifier -message 'Pomodoro'\
         -title 'Work Timer is up! Take a Break 😊'\
         -appIcon '~/Pictures/pumpkin.png'\
