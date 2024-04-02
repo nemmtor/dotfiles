dd4db79098159b2b00b282cbe5a78fbca4c08091
nemmtor <kacper0witas@gmail.com>
2024-04-02 08:39:54 +0200

    add neovim, wezterm and fish

---
+++ nvim/lua/community.lua
@@ -0,0 +1,27 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  -- colorschemes
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- lsp
+  { import = "astrocommunity.lsp.ts-error-translator-nvim" },
+  -- comment
+  { import = "astrocommunity.comment.mini-comment" },
+  -- packs
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.fish" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.go" },
+}
---
+++ nvim/lua/plugins/nvim-window-picker.lua
@@ -0,0 +1,5 @@
+---@type LazySpec
+return {
+  "s1n7ax/nvim-window-picker",
+  opts = function(_, opts) opts.selection_chars = "ABCDEFG" end,
+}
