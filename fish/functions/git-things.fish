3663d849c2076ac22b21d73e38722d23b5c65bc3
nemmtor <kacper0witas@gmail.com>
2025-09-22 21:23:46 +0200

    git things

---
+++ fish/functions/git-things.fish
@@ -0,0 +1,31 @@
+function git-refresh
+    git fetch -p
+end
+
+function git-list-branches
+    git branch -vv
+end
+
+function git-branches-without-remote
+    git-refresh | git-list-branches | grep -E '^\s*\S+\s+[^\[]+$' | awk '{print $1}'
+end
+
+function git-remove-branches-without-remote
+    git-branches-without-remote | xargs git branch -D
+end
+
+function git-branches-remote-gone
+    git-refresh | git-list-branches | grep gone | awk '{print $1}'
+end
+
+function git-remove-branches-remote-gone
+    git-branches-without-remote | xargs git branch -d
+end
+
+function git-what-can-i-remove
+    git clean -n -d
+end
+
+function git-remove-what-can-i-remove-interactive
+    git clean -i
+end
